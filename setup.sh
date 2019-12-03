dea09408383823b2d4d278587e045460c38212ff
joshbosley <bosley117@gmail.com>
2019-12-03 09:46:19 -0500

    Added fuzzy find and root user check

--- setup.sh
+++ setup.sh
@@ -1,10 +1,20 @@
+if [ "$EUID" -eq 0 ]
+  then echo "Do not run as root!"
+  exit
+fi
+
+echo "Starting.."
 
 sudo apt-get update
 
-sudo apt-get install vim guake build-essential htop tmux python3 python3-pip zsh curl wireshark  wget cmake cmake-curses-gui -y
+sudo apt-get install git vim guake build-essential htop tmux python3 python3-pip zsh curl wireshark wget cmake cmake-curses-gui -y
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 source ~/.cargo/env
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+
+~/.fzf/install
